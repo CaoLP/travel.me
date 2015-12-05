72c6a5e90f984ad5dab5429fd25125218ac2e009
Kao <caolpgc60067@gmail.com>
2015-12-05 18:04:46 +0700

    TEST

---
+++ app/src/main/java/app/kao/travel/SplashActivity.java
@@ -0,0 +1,42 @@
+package app.kao.travel;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final ImageView img = (ImageView) findViewById(R.id.loading_img);
+        final Animation animation = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+        final Animation animationOut = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
+
+        img.startAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                img.startAnimation(animationOut);
+                finish();
+                Intent i = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
